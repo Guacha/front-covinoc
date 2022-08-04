e58169c9ea4369be014548feafa3a454429a372b
Guacha <guachamalinga@gmail.com>
2022-08-04 10:05:25 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { UpdateFormComponent } from './components/update-form/update-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    UpdateFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
